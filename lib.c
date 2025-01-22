4309b10755a4810e5c686418fbbb164d740bd3ec
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2025-01-22 11:03:03 +0100

    Add first version of lib.c to test LD_PRELOAD

--- lib.c
+++ lib.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-
-void hello() {
-  printf("Hello World");
+#include <stdlib.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+// #include <security/pam_macros.h>
+//void hello() {
+//  printf("Hello World");
+//}
+int pam_authenticate(pam_handle_t *pamh, int flags){
+    perror("LAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+    return 1;
 }
+
